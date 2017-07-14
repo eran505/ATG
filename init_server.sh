2b1fc8cc562e66c95c904ef488dfbb6e61237b1a
Eran <eran505@gmail.com>
2017-07-14 18:01:48 +0300

    add server script

---
+++ init_server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python evo_call.py /home/ise/eran/repo/common_math/commons-math3-3.6.1-src/target/classes/org/ evosuite-master-1.0.6-SNAPSHOT.jar /home/ise/eran/evosuite/jar/ /home/ise/eran/out/ 120
+
+
+
