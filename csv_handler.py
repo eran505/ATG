42f6967a60c61bcce3992652bf5176373d68d49c
Eran <eran505@gmail.com>
2017-07-16 13:07:12 +0300

    modify the csv

--- csv_handler.py
+++ csv_handler.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import numpy as np
 import os
+import csv
 
 import pandas as pd
 
@@ -24,34 +25,31 @@ def get_all_csv(path):
                         break
                     time=root[w]+time
     list_pd = []
-    new_names=['Covered_Goals','Total_Goals','Coverage']
     for path in list_csv_path:
         pd_tmp = pd.read_csv(path[1])
         index_time = str(path[0])
         list_name = list(pd_tmp)
         size = len(list_name)
-        for i in range(len(new_names)):
-            pd_tmp[new_names[i]+'_'+index_time] = pd_tmp[list_name[size - (i+1)]].copy()
-            pd_tmp.drop(str(list_name[size-(i+1)]), axis=1, inplace=True)
-        list_name = list(pd_tmp)
-        size = len(list_name)
-        fitness=''
-        for i in range(1,size-3,1):
-            fitness=fitness+"_"+str(pd_tmp[list_name[i]][1])
+        for i in range(1,size,1):
+            pd_tmp[str(list_name[i]+'_'+index_time)]=pd_tmp[str(list_name[i])].copy()
             pd_tmp.drop(str(list_name[i]), axis=1, inplace=True)
-        pd_tmp['criterion_' + index_time] = fitness
+        #print list(pd_tmp)
         list_pd.append(pd_tmp)
         pd_tmp=None
     return list_pd
 
-def join_data_frame(d1,d2):
-    if d1.size > d2.size :
-        data_res =  d1.merge(d2 ,how='left', on='TARGET_CLASS')
-    else:
-        data_res = d2.merge(d1, how='left', on='TARGET_CLASS')
+def join_data_frame(list):
+    if len(list)>1 :
+        data_res=list[0]
+        for item in list :
+            if data_res.size > item.size :
+                data_res = data_res.merge(item, how='left', on=' TARGET_CLASS')
+            else:
+                data_res = item.merge(data_res, how='left', on=' TARGET_CLASS')
     return data_res
 
 
 list = get_all_csv(str_path)
-res=join_data_frame(list[0],list[1])
+list.sort(key=lambda x: x.size, reverse=True)
+res=join_data_frame(list)
 res.to_csv("/home/eran/Desktop/evo_result/res.csv", sep='\t', encoding='utf-8')
--- evo_call.py
+++ evo_call.py
@@ -37,6 +37,13 @@ def assemble_path_string2(str) :
     print suffix
     return prefix,suffix
 
+def remove_dot_csv(path):
+    with open(path, 'r') as myfile:
+        data = myfile.read().replace(';','_')
+        text_file = open(path, "w")
+        text_file.write(" %s" % data)
+        text_file.close()
+    return "done"
 
 def get_all_class(root) :
     size=0
@@ -62,23 +69,62 @@ def single_call_EvoSuite(evo_name,evo_path,classes_list,time,dis_path):
     parms4=" -Dtest_dir="+dis_path
 
     all_p = parms1+parms2+parms2+parms3+parms4
-
+    all_command=''
     for cut in classes_list :
         cut_names = str(cut[1]).split('.')
         pre,suf = assemble_path_string2(cut[0])
         test = suf  + cut_names[0]
         command = evo_string + " -class " +test+" -projectCP "+pre+criterion+all_p
         print command
+        all_command = all_command +'\n'*2 + command
         os.system(command)
-        text_file = open(dis_path+"command.txt", "w")
-        text_file.write("command: %s" % command)
-        text_file.close()
+    text_file = open(dis_path + "command.txt", "w")
+    text_file.write("command: \n  %s" % all_command)
+    text_file.close()
+    remove_dot_csv(dis_path+'statistics.csv')
+
+def exp_test(evo_name,evo_path,classes_list,time,dis_path,names) :
+    cuts = []
+    for cut in classes_list:
+        for name in names:
+            if cut[1] == name:
+                cuts.append(cut)
+
+    exp_test_evo(evo_name,evo_path,cuts,time,dis_path)
+
+def exp_test_evo(evo_name,evo_path,classes_list,time,dis_path):
+    evo_string = "java -jar " + evo_path +evo_name
+    criterions = ['none','LINE', 'BRANCH', 'EXCEPTION' ]
+    parms1="-Dsearch_budget="+time
+    parms2=" -Dglobal_timeout="+time
+    parms3=" -Dreport_dir="+dis_path
+    parms4=" -Dtest_dir="+dis_path
+    parms5=" -Doutput_variables=TARGET_CLASS,criterion,Size,Length,MutationScore,Total_Time,Covered_Goals,Total_Goals,Coverage"
+
+    all_p = parms1+parms3+parms4+parms5+parms2
+    all_command=''
+    for cut in classes_list :
+        for c in criterions:
+            criterion = " -criterion "+c+" "
+            cut_names = str(cut[1]).split('.')
+            pre,suf = assemble_path_string2(cut[0])
+            test = suf  + cut_names[0]
+            if c == 'none':
+                command = evo_string + " -class " +test+" -projectCP "+pre+" "+all_p
+            else:
+                command = evo_string + " -class " + test + " -projectCP " + pre + criterion + all_p
+            all_command=all_command+'\n'+command
+            print command
+            os.system(command)
+    text_file = open(dis_path+"command.txt", "w")
+    text_file.write("commands: \n %s" % all_command)
+    text_file.close()
+    remove_dot_csv(dis_path+'statistics.csv')
 
 
-
-#str_commands = "evo_call.py /home/eran/thesis/projects-ex/commons-math3-3.6.1-src/target/classes/org/ evosuite-master-1.0.6-SNAPSHOT.jar /home/eran/programs/EVOSUITE/jar/ /home/eran/Desktop/evo_result/ 10"
-#str_arry = str_commands.split(" ")
-#sys.argv = str_arry
+str_commands = "evo_call.py /home/eran/thesis/projects-ex/commons-math3-3.6.1-src/target/classes/org/ evosuite-master-1.0.6-SNAPSHOT.jar /home/eran/programs/EVOSUITE/jar/ /home/eran/Desktop/evo_result/ 60"
+str_arry = str_commands.split(" ")
+sys.argv = str_arry
 
 if len(sys.argv) == 6 :
     v_path = sys.argv[1]
@@ -93,6 +139,7 @@ if len(sys.argv) == 6 :
         print 'cant make dir '
         exit(1)
     target_list = get_all_class(v_path)
+    #exp_test(v_evo_name,v_evo_path,target_list,v_time,full_dis,['AdamsFieldStepInterpolator.class'])
     single_call_EvoSuite(v_evo_name,v_evo_path,target_list,v_time,full_dis)
     print ("done !")
 else:
