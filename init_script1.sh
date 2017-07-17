8dbed78d445c012523da4d624b4eeff0ac4c60a1
eran505 <eran505@gmail.com>
2017-07-17 21:28:28 +0300

    Delete init_script1.sh

--- init_script1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python evo_call.py /home/eran/thesis/projects-ex/commons-math3-3.6.1-src/target/classes/org/ evosuite-master-1.0.6-SNAPSHOT.jar /home/eran/programs/EVOSUITE/jar/ /home/eran/Desktop/evo_result/ 10
-
-
-
