d14e6bc3835cbd7851a96cb982625787357fff12
eran505 <eran505@gmail.com>
2017-07-17 21:28:10 +0300

    Add files via upload

--- evo_call.py
+++ evo_call.py
@@ -37,6 +37,25 @@ def assemble_path_string2(str) :
     print suffix
     return prefix,suffix
 
+def mereg_dic(list_new,list_org):
+    if len(list_new) > len(list_org):
+        size=len(list_org)
+    else:
+        size= len(list_new)
+    dic_new={}
+    dic_org={}
+    for item_new in list_new :
+        cut_names = str(item_new[1]).split('.')
+        new_path,suf = assemble_path_string2(item_new[0])
+        cut = suf  + cut_names[0]
+        dic_new[cut]=str(new_path)
+    for item_org in list_org:
+        cut_names = str(item_org[1]).split('.')
+        new_org,suf = assemble_path_string2(item_org[0])
+        cut = suf  + cut_names[0]
+        dic_org[cut]=str(new_org)
+    return dic_new,dic_org
+
 def remove_dot_csv(path):
     with open(path, 'r') as myfile:
         data = myfile.read().replace(';','_')
@@ -62,19 +81,23 @@ def single_call_EvoSuite(evo_name,evo_path,classes_list,time,dis_path):
 
     evo_string = "java -jar " + evo_path +evo_name
 
-    criterion = " -criterion EXCEPTION "
+    criterion = " "
+
+    depand = ":/home/eran/thesis/Tutorial_Experiments/target/dependency/commons-collections-3.2.2.jar"
     parms1="-Dsearch_budget="+time
-    parms2=" -Dglobal_timeout="+time
+   # parms2=" -Dglobal_timeout="+time
     parms3=" -Dreport_dir="+dis_path
     parms4=" -Dtest_dir="+dis_path
+    parms5=" -Doutput_variables=TARGET_CLASS,criterion,Lines,Covered_Lines,Total_Methods,Covered_Methods,\
+Total_Branches,Covered_Branches,ExceptionCoverage,Size,Length,MutationScore,Total_Time,Covered_Goals,Total_Goals,Coverage"
 
-    all_p = parms1+parms2+parms2+parms3+parms4
+    all_p = parms1+parms3+parms4+parms5
     all_command=''
     for cut in classes_list :
         cut_names = str(cut[1]).split('.')
         pre,suf = assemble_path_string2(cut[0])
         test = suf  + cut_names[0]
-        command = evo_string + " -class " +test+" -projectCP "+pre+criterion+all_p
+        command = evo_string + " -class " +test+" -projectCP "+pre+depand+criterion+all_p
         print command
         all_command = all_command +'\n'*2 + command
         os.system(command)
@@ -83,48 +106,47 @@ def single_call_EvoSuite(evo_name,evo_path,classes_list,time,dis_path):
     text_file.close()
     remove_dot_csv(dis_path+'statistics.csv')
 
-def exp_test(evo_name,evo_path,classes_list,time,dis_path,names) :
-    cuts = []
-    for cut in classes_list:
-        for name in names:
-            if cut[1] == name:
-                cuts.append(cut)
 
-    exp_test_evo(evo_name,evo_path,cuts,time,dis_path)
+def regression_test(evo_name, evo_path, classes_list_new,classes_list_original,time, dis_path) :
+    evo_string = "java -jar " + evo_path + evo_name +" -regressionSuite "
 
-def exp_test_evo(evo_name,evo_path,classes_list,time,dis_path):
-    evo_string = "java -jar " + evo_path +evo_name
-    criterions = ['none','LINE', 'BRANCH', 'EXCEPTION' ]
-    parms1="-Dsearch_budget="+time
-    parms2=" -Dglobal_timeout="+time
-    parms3=" -Dreport_dir="+dis_path
-    parms4=" -Dtest_dir="+dis_path
-    parms5=" -Doutput_variables=TARGET_CLASS,criterion,Size,Length,MutationScore,Total_Time,Covered_Goals,Total_Goals,Coverage"
+    #parms0 = " -Dregression_skip_similar=true"
+    parms1 = " -Dsearch_budget=" + time
+    parms2=" -Dregression_statistics=true"
+    parms3 = " -Dreport_dir=" + dis_path
+    parms4 = " -Dtest_dir=" + dis_path
+    all_p = parms1+parms2+parms3+parms4
 
-    all_p = parms1+parms3+parms4+parms5+parms2
-    all_command=''
-    for cut in classes_list :
-        for c in criterions:
-            criterion = " -criterion "+c+" "
-            cut_names = str(cut[1]).split('.')
-            pre,suf = assemble_path_string2(cut[0])
-            test = suf  + cut_names[0]
-            if c == 'none':
-                command = evo_string + " -class " +test+" -projectCP "+pre+" "+all_p
-            else:
-                command = evo_string + " -class " + test + " -projectCP " + pre + criterion + all_p
-            all_command=all_command+'\n'+command
-            print command
-            os.system(command)
-    text_file = open(dis_path+"command.txt", "w")
-    text_file.write("commands: \n %s" % all_command)
+    dic_new,dic_org = mereg_dic(classes_list_new,classes_list_original)
+    all_command = ''
+    for key, value in dic_new.iteritems():
+        if key in dic_org :
+            org_p=str(dic_org[key])
+        else:
+            continue
+        new_p = str(value)
+        if len(org_p)<1 :
+            continue
+        command = evo_string + " -class " +key+" -projectCP "+org_p+" -Dregressioncp="+new_p+all_p
+        print command
+        all_command = all_command +'\n'*2 + command
+        os.system(command)
+    text_file = open(dis_path + "command.txt", "w")
+    text_file.write("command: \n  %s" % all_command)
     text_file.close()
-    remove_dot_csv(dis_path+'statistics.csv')
+    #remove_dot_csv(dis_path+'statistics.csv')
 
 
-str_commands = "evo_call.py /home/eran/thesis/projects-ex/commons-math3-3.6.1-src/target/classes/org/ evosuite-master-1.0.6-SNAPSHOT.jar /home/eran/programs/EVOSUITE/jar/ /home/eran/Desktop/evo_result/ 60"
-str_arry = str_commands.split(" ")
-sys.argv = str_arry
+
+
+#str_regg = "evo_call.py /home/eran/thesis/common_math/commons-math3-3.5-src/target/classes/org/ /home/eran/thesis/common_math/commons-math3-3.6.1-src/target/classes/org/ evosuite-1.0.5.jar /home/eran/programs/EVOSUITE/jar/ /home/eran/Desktop/evo_result/ 30"
+
+#str_commands_common_math = "evo_call.py /home/eran/thesis/projects-ex/commons-math3-3.6.1-src/target/classes/org/ evosuite-1.0.5.jar /home/eran/programs/EVOSUITE/jar/ /home/eran/Desktop/evo_result/ 60"
+
+#str_command_tut = "evo_call.py /home/eran/thesis/Tutorial_Experiments/target/classes/tutorial/ evosuite-1.0.5.jar /home/eran/programs/EVOSUITE/jar/ /home/eran/Desktop/out_exp/ 10"
+
+#str_arry = str_regg.split(" ")
+#sys.argv = str_arry
 
 if len(sys.argv) == 6 :
     v_path = sys.argv[1]
@@ -139,29 +161,29 @@ if len(sys.argv) == 6 :
         print 'cant make dir '
         exit(1)
     target_list = get_all_class(v_path)
-    #exp_test(v_evo_name,v_evo_path,target_list,v_time,full_dis,['AdamsFieldStepInterpolator.class'])
     single_call_EvoSuite(v_evo_name,v_evo_path,target_list,v_time,full_dis)
     print ("done !")
 else:
-    print "miss argv (-Path -EvoName -EvoPath -DistPath -Time)"
+    if len(sys.argv) == 7 :
+        v_path_org = sys.argv[1]
+        v_path_new = sys.argv[2]
+        v_evo_name = sys.argv[3]
+        v_evo_path = sys.argv[4]
+        v_dis_path = sys.argv[5]
+        v_time =     sys.argv[6]
+        localtime = time.asctime(time.localtime(time.time()))
+        localtime_str = str(localtime) + '_t=' + str(v_time)
+        full_dis = mkdir_Os(v_dis_path, localtime_str)
+        if full_dis == 'null':
+            print 'cant make dir '
+            exit(1)
+        list_org = get_all_class(v_path_org)
+        list_new = get_all_class(v_path_new)
+        regression_test(v_evo_name, v_evo_path, list_new, list_org, v_time, full_dis)
+        print 'Done !'
 
 
 
+print "miss argv (-Path -EvoName -EvoPath -DistPath -Time)"
 
-path_1 = "/home/eran/thesis/Tutorial/Tutorial_Experiments/target/classes/tutorial/"
 
-path_2 = "/home/eran/thesis/projects-ex/commons-math3-3.6.1-src/target/classes/org/"
-
-evo_path="/home/eran/programs/EVOSUITE/jar/"
-
-evo_name = "evosuite-1.0.4.jar"
-
-evo_name_sanpshot = "evosuite-master-1.0.6-SNAPSHOT.jar"
-
-evo_st_name ="evosuite-standalone-runtime-1.0.4.jar"
-
-dis_path = "/home/eran/Desktop/evo_result/"
-
-#target_list = get_all_class(path_2)
-
-#single_call_EvoSuite(evo_name_sanpshot,evo_path,target_list,'5',dis_path)
---
+++ rr_init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+python evo_call.py /home/ise/eran/repo/common_math/commons-math3-3.5-src/target/classes/org/ /home/ise/eran/repo/common_math/commons-math3-3.6.1-src/target/classes/org/ evosuite-1.0.5.jar /home/ise/eran/evosuite/jar/ /home/ise/eran/out/ $1
+
+
+
