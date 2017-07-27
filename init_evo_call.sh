39fc96dbac414db63cd9c9138e6b9b5ba6a20b8f
Eran <eran505@gmail.com>
2017-07-27 15:23:14 +0300

    init changes

--- init_evo_call.sh
+++ init_evo_call.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+read -p "Version Enter: " VAR
 
-python evo_call.py /home/ise/eran/repo/common_math/commons-math3-3.6-src/target/classes/org/ evosuite-1.0.5.jar /home/ise/eran/evosuite/jar/ /home/ise/eran/out/ $1
+
+
+read -p "Time Enter: " time
+
+
+python evo_call.py /home/ise/eran/repo/common_math/commons-math3-${VAR}-src/target/classes/org/ evosuite-1.0.5.jar /home/ise/eran/evosuite/jar/ /home/ise/eran/out/ ${time}
