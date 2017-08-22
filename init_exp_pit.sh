398825f5edd644fbbc70a62207b912785f949d0c
Eran <eran505@gmail.com>
2017-08-22 05:15:04 +0300

    add

--- init_exp_pit.sh
+++ init_exp_pit.sh
@@ -9,6 +9,7 @@ out_path=/home/ise/eran/out/pit_mvn/
 mv $curdir/src/test/java/org/ $newdir
 cp -avr $curdir/.evosuite/best-tests/org $curdir/src/test/java/
 mvn clean test
+exit
 mvn org.pitest:pitest-maven:mutationCoverage
 mv $curdir/target/pit-reports/* "$out_path"
 
