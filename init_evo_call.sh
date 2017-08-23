f02169b5962780d41c8ea694fc147d6e2f0cd984
Eran <eran505@gmail.com>
2017-08-23 04:03:45 +0300

    aa

--- init_evo_call.sh
+++ init_evo_call.sh
@@ -1,10 +1,5 @@
 #!/bin/bash 
 
-if [ -z "$1" ]; then
-echo "not time insert"
-exit
-fi 
 
-dir=${1}_sec
 
-python evo_call.py /home/ise/eran/repo/common_math/commons-math3-3.5-src/target/classes/org/ evosuite-1.0.5.jar /home/ise/eran/evosuite/jar/ /home/ise/eran/out/${dir}/ ${1}
+python budget_generation.py /home/ise/eran/repo/common_math/commons-math3-3.5-src/target/classes/org/ evosuite-1.0.5.jar /home/ise/eran/evosuite/jar/ /home/ise/eran/out/ budget.csv
