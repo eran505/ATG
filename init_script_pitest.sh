d53b8ab84b025b11a4cd9f2e8b7b8ac3dbea405a
Eran <eran505@gmail.com>
2017-08-30 01:00:27 +0300

    pom

--- init_script_pitest.sh
+++ init_script_pitest.sh
@@ -13,7 +13,9 @@ mkdir ${string_dir}
 tar -xzvf /home/ise/eran/repo/common_math/jars/commons-math3-3.5-src.tar.gz -C /home/ise/eran/exp/${string_dir}
 relative_path=/home/ise/eran/exp/${string_dir}/commons-math3-3.5-src
 rm -r /home/ise/eran/exp/${string_dir}/commons-math3-3.5-src/src/test/java/org
+rm /home/ise/eran/exp/${string_dir}/commons-math3-3.5-src/pom.xml
 cp -avr ${path_arg} ${relative_path}/src/test/java/
+cp -avr /home/ise/eran/repo/ATG/pom.xml /home/ise/eran/exp/${string_dir}/commons-math3-3.5-src/
 cd ${relative_path}
 mvn install 
 mvn org.pitest:pitest-maven:mutationCoverage
