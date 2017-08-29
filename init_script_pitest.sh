23a60e437735b7c2170e312feb7ac6359a0bd96e
Eran <eran505@gmail.com>
2017-08-30 00:46:06 +0300

    bla

--- init_script_pitest.sh
+++ init_script_pitest.sh
@@ -2,17 +2,16 @@
 
 
 path_arg=${1}
-
 if [ -z "$path_arg" ]; then
 echo "missing path value "
 exit
 fi
-now_time=$(date +%T)
-string_dir="$now_time" | tr : _
-string_dir=${string_dir}"_V"${RANDOM}
+
+string_dir="V_"${RANDOM}
+
 mkdir $string_dir
-tar -xzvf /home/ise/eran/repo/common_math/jar/commons-math3-3.5-src.tar.gz -C /home/ise/eran/exp/${$string_dir}
-relativ_path=/home/ise/eran/exp/${$string_dir}/commons-math3-3.5-src/
+tar -xzvf /home/ise/eran/repo/common_math/jars/commons-math3-3.5-src.tar.gz -C /home/ise/eran/exp/${$string_dir}
+relativ_path=/home/ise/eran/exp/${$string_dir}/commons-math3-3.5-src
 rm -r /home/ise/eran/exp/${$string_dir}/commons-math3-3.5-src/src/test/java/org
 cp -avr ${path_arg} ${relativ_path}/src/test/java/
 cd ${relativ_path}
