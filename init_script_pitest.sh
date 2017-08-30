189c3dddb53b7d4b5e39bc3b647af363331e0ff9
Eran <eran505@gmail.com>
2017-08-31 01:09:34 +0300

    31:08:01:09

--- init_script_pitest.sh
+++ init_script_pitest.sh
@@ -10,13 +10,13 @@ fi
 string_dir="ALL_"${RANDOM}
 
 mkdir ${string_dir}
-tar -xzvf /home/ise/eran/repo/common_math/jars/commons-math3-3.5-src.tar.gz -C /home/ise/eran/exp/${string_dir}
-relative_path=/home/ise/eran/exp/${string_dir}/commons-math3-3.5-src
-rm -r /home/ise/eran/exp/${string_dir}/commons-math3-3.5-src/src/test/java/org
-rm /home/ise/eran/exp/${string_dir}/commons-math3-3.5-src/pom.xml
+tar -xzvf /home/ise/eran/repo/common_math/jars/commons-math3-3.5-src.tar.gz -C /home/ise/eran/exp_all/${string_dir}
+relative_path=/home/ise/eran/exp_all/${string_dir}/commons-math3-3.5-src
+rm -r /home/ise/eran/exp_all/${string_dir}/commons-math3-3.5-src/src/test/java/org
+rm /home/ise/eran/exp_all/${string_dir}/commons-math3-3.5-src/pom.xml
 cp -avr ${path_arg} ${relative_path}/src/test/java/
-cp -avr /home/ise/eran/repo/ATG/pom.xml /home/ise/eran/exp/${string_dir}/commons-math3-3.5-src/
-cp -avr /home/ise/eran/repo/ATG/pti_init.py /home/ise/eran/exp/${string_dir}/commons-math3-3.5-src/
+cp -avr /home/ise/eran/repo/ATG/pom.xml /home/ise/eran/exp_all/${string_dir}/commons-math3-3.5-src/
+cp -avr /home/ise/eran/repo/ATG/pti_init.py /home/ise/eran/exp_all/${string_dir}/commons-math3-3.5-src/
 cd ${relative_path}
 mvn install
 python pti_init.py
