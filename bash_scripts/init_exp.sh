d9d4d7408ad9f25bb875dd5ebe7b67e4e13a4b9e
Eran <eran505@gmail.com>
2017-09-27 16:30:24 +0300

    09/27 16:30:24

--- bash_scripts/init_exp.sh
+++ bash_scripts/init_exp.sh
@@ -32,12 +32,11 @@ fi
 cd "$newdir"
 mkdir csv
 cp -avr ${ATG}csv/Most_out_files.csv ./csv/
-python ${ATG}budget_generation.py /home/ise/eran/repo/common_math/commons-math3-3.5-src/target/classes/org/apache/commons/math3/linear/ evosuite-1.0.5.jar /home/ise/eran/evosuite/jar/ ${newdir} ${m} 30 180 ${t}
-#if [[ $cur_dir == *"ise"* ]]; then
-#	python ${ATG}budget_generation.py /home/ise/eran/repo/common_math/commons-math3-3.5-src/target/classes/org/apache/commons/math3/linear/ evosuite-1.0.5.jar /home/ise/eran/evosuite/jar/ ${newdir} ${m} 30 180 ${t}
-#else
-#	python  ${ATG}budget_generation.py  /home/eran/thesis/test_gen/poc/commons-math3-3.5-src/target/classes/org/apache/commons/math3/fraction/ evosuite-1.0.5.jar /home/eran/programs/EVOSUITE/jar/ ${newdir} ${m} 30 180 ${t}
-#fi
+if [[ $cur_dir == *"ise"* ]]; then
+	python ${ATG}budget_generation.py /home/ise/eran/repo/common_math/commons-math3-3.5-src/target/classes/org/apache/commons/math3/linear/ evosuite-1.0.5.jar /home/ise/eran/evosuite/jar/ ${newdir} ${m} 30 180 ${t}
+else
+	python  ${ATG}budget_generation.py  /home/eran/thesis/test_gen/poc/commons-math3-3.5-src/target/classes/org/apache/commons/math3/fraction/ evosuite-1.0.5.jar /home/eran/programs/EVOSUITE/jar/ ${newdir} ${m} 30 180 ${t}
+fi
 python ${ATG}pit_render_test.py ${newdir}
 mkdir "pit_test"
 cp ${newdir}FP_budget_time.csv ${pitest}pit_test/
