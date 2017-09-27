39cbea603b47014da38047de5d8f349df2a7ed97
Eran <eran505@gmail.com>
2017-09-27 15:48:55 +0300

    bal

--- bash_scripts/init_exp.sh
+++ bash_scripts/init_exp.sh
@@ -33,7 +33,7 @@ cd "$newdir"
 mkdir csv
 cp -avr ${ATG}csv/Most_out_files.csv ./csv/
 if [[ $cur_dir == *"ise"* ]]; then
-	python ${ATG}budget_generation.py /home/ise/eran/repo/common_math/commons-math3-3.5-src/target/classes/org/apache/commons/math3/distribution/ evosuite-1.0.5.jar /home/ise/eran/evosuite/jar/ ${newdir} ${m} 30 180 ${t}
+	python ${ATG}budget_generation.py /home/ise/eran/repo/common_math/commons-math3-3.5-src/target/classes/org/apache/commons/math3/linear/ evosuite-1.0.5.jar /home/ise/eran/evosuite/jar/ ${newdir} ${m} 30 180 ${t}
 else
 	python  ${ATG}budget_generation.py  /home/eran/thesis/test_gen/poc/commons-math3-3.5-src/target/classes/org/apache/commons/math3/fraction/ evosuite-1.0.5.jar /home/eran/programs/EVOSUITE/jar/ ${newdir} ${m} 30 180 ${t}
 
