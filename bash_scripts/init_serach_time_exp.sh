6c437f0eb5d41f9e90971ce782dbb4c93eb60afa
Eran <eran505@gmail.com>
2017-10-31 14:54:38 +0200

    10/31 14:54:38

--- bash_scripts/init_serach_time_exp.sh
+++ bash_scripts/init_serach_time_exp.sh
@@ -2,5 +2,13 @@
 
 location_dir=$1
 
+ctr=10
 
-bash serach_time_exp.sh
+while [ $ctr -le 300 ]
+    do
+        echo "generate testing with time budget= $ctr"
+        des_dir=${location_dir}/T_${ctr}/
+        mkdir ${location_dir}/T_${ctr}/
+        #bash serach_time_exp.sh ${des_dir} ${ctr} ~/eran/repo/ATG/
+        ((ctr=ctr+10))
+    done
