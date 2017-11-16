d2f563ae19aef809eded7948b2573e4c1e0bb137
Eran <eran505@gmail.com>
2017-11-16 23:09:42 +0200

    11/16 23:09:42

--- bash_scripts/vtest.sh
+++ bash_scripts/vtest.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 
-bash init_exp.sh ~/eran/repo/ATG/ 30 exp
+bash init_exp.sh ~/eran/repo/ATG/ 10 exp
 
-bash init_exp.sh ~/eran/repo/ATG/ 40 exp
+bash init_exp.sh ~/eran/repo/ATG/ 30 exp
 
 bash init_exp.sh ~/eran/repo/ATG/ 60 exp
 
-bash init_exp.sh ~/eran/repo/ATG/ 35 exp
+bash init_exp.sh ~/eran/repo/ATG/ 90 exp
 
+bash init_exp.sh ~/eran/repo/ATG/ 120 exp
