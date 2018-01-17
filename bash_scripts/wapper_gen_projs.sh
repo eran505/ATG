eb0e5560f2ba1dec36fa5d77d38e40ce80bade1a
eran505 <eran505@gmail.com>
2018-01-17 16:59:27 +0200

    01/17 16:59:27

--- bash_scripts/wapper_gen_projs.sh
+++ bash_scripts/wapper_gen_projs.sh
@@ -25,5 +25,6 @@ echo "found: "${cnt}
 for ((i=0;i<cnt;i++)); do
 	path_dir=${array[i]}
 	cp ${ATG}bash_scripts/mutation_test.sh ${path_dir}
+	cd ${path_dir}
 	bash ${path_dir}/mutation_test.sh
 done
