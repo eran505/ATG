448c68840af7e946a0238f15a400732e9492b7f2
eran505 <eran505@gmail.com>
2018-02-12 17:40:58 +0200

    02/12 17:40:58

--- bash_scripts/vtest.sh
+++ bash_scripts/vtest.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
 
-
-bash init_exp.sh ~/eran/repo/ATG/ 10 exp
-
-bash init_exp.sh ~/eran/repo/ATG/ 8 exp
-
-bash init_exp.sh ~/eran/repo/ATG/ 4 exp
-
+bash init_exp.sh ~/eran/repo/ATG/ 1 exp
 bash init_exp.sh ~/eran/repo/ATG/ 2 exp
-
-bash init_exp.sh ~/eran/repo/ATG/ 6 exp
+bash init_exp.sh ~/eran/repo/ATG/ 4 exp
+bash init_exp.sh ~/eran/repo/ATG/ 5 exp
+bash init_exp.sh ~/eran/repo/ATG/ 7 exp
+bash init_exp.sh ~/eran/repo/ATG/ 9 exp
 
 
 
