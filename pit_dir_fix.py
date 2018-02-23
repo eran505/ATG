fab704a48554cb7a5dcc0447f50266f1290c87f9
eran505 <eran505@gmail.com>
2018-02-23 17:13:58 +0200

    02/23 17:13:58

--- pit_dir_fix.py
+++ pit_dir_fix.py
@@ -6,7 +6,7 @@ import sys
 from time import gmtime, strftime
 
 
-def get_all_bugs_dir(pit_dir_path):
+def get_all_bugs_dir(pit_dir_path,arg):
     script_py = extract_script(pit_dir_path)
     print script_py
     obj_walk = walker(pit_dir_path)
@@ -27,9 +27,9 @@ def get_all_bugs_dir(pit_dir_path):
                 bugs_dir.append([dir,name_class_dir])
         else:
             empty_dir.append([dir,name_class_dir])
-    fix_error_list(bugs_dir,script_py,'bug_dir')
-    fix_error_list(empty_csv, script_py,'empty_csv')
-    fix_error_list(empty_dir, script_py,'empty_dir')
+    fix_error_list(bugs_dir,script_py,'bug_dir',arg)
+    fix_error_list(empty_csv, script_py,'empty_csv',arg)
+    fix_error_list(empty_dir, script_py,'empty_dir',arg)
 
 def extract_script(p):
     arr = str(p).split('/')
@@ -42,15 +42,17 @@ def extract_script(p):
         res.append(string)
     return '/'.join(res)
 
-def fix_error_list(list_err_dir,root_dir,name_file):
+def fix_error_list(list_err_dir,root_dir,name_file,arg):
     log_to_dir(root_dir,list_err_dir,name_file)
     for item in list_err_dir: #pti_init.py
         del_dir(item[0],item[1],root_dir+"/target/log_pit/")
         print "class: ",item[1]
         os.chdir(root_dir)
         py_script =root_dir+'/pti_init.py'
-        os.system("python {} {}".format(py_script,item[1]))
-
+        if arg is None:
+            os.system("python {} {}".format(py_script,item[1]))
+        else:
+            os.system("python {} {} {}".format(py_script, item[1],arg))
 
 def log_to_dir(root_path,li,name_file):
     if os.path.isdir(root_path+'/logs') is False:
@@ -96,13 +98,13 @@ def is_empty_dir(path):
 
 
 
-def get_all_pit_dir_exp(root_exp):
+def get_all_pit_dir_exp(root_exp,arg=None):
     obj = walker(root=root_exp)
     all_pp = obj.walk('pit-reports',False)
     if len(all_pp)==0:
         print "No dir pit-report dir in the following path : {}".format(root_exp)
     for p in all_pp:
-        get_all_bugs_dir(p)
+        get_all_bugs_dir(p,arg)
 
 
 
@@ -132,4 +134,9 @@ if __name__ == "__main__":
     #pp_path = '/home/ise/Desktop/test_50/pit_test/ALL_FP__t=50_it=1/commons-math3-3.5-src/target/pit-reports/'
     #get_all_bugs_dir(pp_path)
     args = sys.argv
-    get_all_pit_dir_exp(args[1])
+    if len(args)==2:
+        get_all_pit_dir_exp(args[1])
+    elif len(args)==3:
+        get_all_pit_dir_exp(args[1],args[2])
+    else:
+        print "exit ---> no args !!!"
