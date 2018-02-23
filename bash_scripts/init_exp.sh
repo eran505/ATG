85f8a53e4f3cc3b3428c65510fee20272267b660
eran505 <eran505@gmail.com>
2018-02-23 14:49:17 +0200

    02/23 14:49:17

--- bash_scripts/init_exp.sh
+++ bash_scripts/init_exp.sh
@@ -33,7 +33,7 @@ fi
 cd "$newdir"
 mkdir csv
 cp -avr ${ATG}csv/Most_out_files.csv ./csv/
-python ${ATG}budget_generation.py /home/ise/eran/repo/common_math/commons-math3-3.5-src/target/classes/org/apache/commons/math3/ evosuite-1.0.5.jar /home/ise/eran/evosuite/jar/ ${newdir} ${m} 1 300 ${t}
+python ${ATG}budget_generation.py /home/ise/eran/repo/common_math/commons-math3-3.5-src/target/classes/org/apache/commons/math3/ evosuite-1.0.5.jar /home/ise/eran/evosuite/jar/ ${newdir} ${m} 1 240 ${t}
 python ${ATG}pit_render_test.py ${newdir}
 
 cd ${main_dir}
