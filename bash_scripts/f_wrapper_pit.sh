13a7f07635aa38d67fa2031066a203e316611b47
eranhe <eran505@gmail.com>
2018-04-04 13:27:48 +0300

    04/04 13:27:48

---
+++ bash_scripts/f_wrapper_pit.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+
+root_dir=${PWD}
+arg_package=${1}
+
+ATG="/home/ise/eran/repo/ATG/"
+Mathjar="/home/ise/eran/repo/common_math/jars/commons-math3-3.5-src.tar.gz"
+father_dir=${PWD}
+${ATG}python pit_render_test.py ${PWD}
+for D in `find ${PWD}  -maxdepth 1  -type d  `
+do
+
+    dir_name=${D##*/}
+    if [[ $dir_name == *"_t="* ]]; then
+
+	    string=${D}
+	    array+=(${D})
+	fi
+
+done
+
+str_data=$(date +'%m_%d__%H_%M_%S')
+file_log="log_fix_${str_data}_.txt"
+if [ ! -d "${PWD}/logs" ]; then
+  mkdir "${PWD}/logs"
+fi
+
+touch "${PWD}/logs/"${file_log}
+
+file_log=${PWD}"/logs/"${file_log}
+
+cnt=${#array[@]}
+for ((i=0;i<cnt;i++)); do
+	array_sub=()
+	father_dir=${array[i]}
+	if [ ! -d "${father_dir}/pit_test" ]; then
+		mkdir "${father_dir}/pit_test"
+	fi
+	for D in `find ${father_dir}  -maxdepth 1  -type d  `
+	do
+		currnet_dir=${D##*/}
+		if [[ $currnet_dir == *"exp"* ]]; then
+	    	string=${D}
+	    	array_sub+=(${D})
+		fi
+	done
+	#suffix_name=${path_dir##*/}
+	#echo "path_dir"$path_dir
+	#echo "path_dir="$suffix_name
+	cnt=${#array_sub[@]}
+	for ((k=0;k<cnt;k++)); do
+		now_dir=${array_sub[k]}
+		echo "now="$now_dir
+		suffix_name=${now_dir##*/}
+		echo "suffix_name="$suffix_name
+		mod=$(echo $suffix_name| cut -d'_' -f 1)
+		echo "mod="$mod
+		time=$(echo $suffix_name| cut -d'_' -f 9)
+		echo "time="$time
+		it=$(echo $suffix_name| cut -d'_' -f 10)
+		echo "it="$it
+		string_dir="ALL_"${mod}"_${time}_"${it}"_"
+		echo $string_dir
+		cut_dir=${father_dir}/"pit_test"/${string_dir}
+		mkdir $cut_dir
+		tar -xzf ${Mathjar} -C $cut_dir
+		relative_path=${cut_dir}/commons-math3-3.5-src/
+		rm -r ${cut_dir}/commons-math3-3.5-src/src/test/java/org
+		rm ${cut_dir}/commons-math3-3.5-src/pom.xml
+		cp -ar ${now_dir}/org/ ${relative_path}src/test/java/
+		cp -ar ${ATG}pom.xml ${relative_path}
+		cp -ar ${ATG}pti_init.py ${relative_path}
+		cd ${relative_path}
+		mvn -fn install
+
+	done
+
+done
+
+##########################3
+
+python ${ATG}pit_render_test.py ${root_dir}
+python ${ATG}fail_cleaner.py ${root_dir}
+cd ${root_dir}
+bash fixer.sh
--- pit_render_test.py
+++ pit_render_test.py
@@ -1,10 +1,11 @@
-import os,re,sys
+import os, re, sys
+
 
 class walker:
     def __init__(self, root):
         self._root = root
 
-    def walk(self,rec="", file_t=True, lv=-1, full=True):
+    def walk(self, rec="", file_t=True, lv=-1, full=True):
         size = 0
         ctr = 0
         class_list = []
@@ -37,17 +38,17 @@ class walker:
         return class_list
 
 
-def walk(root,rec="",file_t=True,lv=-1,full=True) :
-    size=0
-    ctr=0
+def walk(root, rec="", file_t=True, lv=-1, full=True):
+    size = 0
+    ctr = 0
     class_list = []
     if lv == -1:
-        lv=float('inf')
+        lv = float('inf')
     for path, subdirs, files in os.walk(root):
         if lv < ctr:
             break
-        ctr+=1
-        if file_t :
+        ctr += 1
+        if file_t:
             for name in files:
                 tmp = re.compile(rec).search(name)
                 if tmp == None:
@@ -70,17 +71,16 @@ def walk(root,rec="",file_t=True,lv=-1,full=True) :
     return class_list
 
 
-def walk_rec(root,rec="",file_t=True,lv=-1,full=True) :
-    size=0
-    ctr=0
-    class_list = []
-    if lv == -1:
-        lv=float('inf')
+def walk_rec(root, list_res, rec="", file_t=True, lv=-1, full=True):
+    size = 0
+    ctr = 0
+    class_list = list_res
+    if lv == 0:
+        return list_res
+    lv += 1
     for path, subdirs, files in os.walk(root):
-        if lv < ctr:
-            break
-        ctr+=1
-        if file_t :
+        ctr += 1
+        if file_t:
             for name in files:
                 tmp = re.compile(rec).search(name)
                 if tmp == None:
@@ -100,11 +100,14 @@ def walk_rec(root,rec="",file_t=True,lv=-1,full=True) :
                     class_list.append(os.path.join(path, name))
                 else:
                     class_list.append(str(name))
+        for d_dir in subdirs:
+            walk_rec("{}/{}".format(path, d_dir), class_list, rec, file_t, lv, full)
+        break
     return class_list
 
 
 def fix_bug_tests(root):
-    all_test= walk(root,"ESTest.java")
+    all_test = walk(root, "ESTest.java")
     for cut in all_test:
         with open(cut, 'r') as myfile:
             data = myfile.read().replace('separateClassLoader = true', 'separateClassLoader = false')
@@ -113,36 +116,41 @@ def fix_bug_tests(root):
         text_file.close()
     print "done !"
 
-def path_to_package(first,path,cut):
-    arr=[]
-    dim=0
-    while(path.find(first,dim) != -1):
-        arr.append(path.find(first,dim))
-        dim = path.find(first,dim) + len(first)
-    if len(arr) != 1 :
-        raise Exception('confilcet in path to package :' ,path, "with the key:",first  )
+
+def path_to_package(first, path, cut):
+    arr = []
+    dim = 0
+    while (path.find(first, dim) != -1):
+        arr.append(path.find(first, dim))
+        dim = path.find(first, dim) + len(first)
+    if len(arr) != 1:
+        raise Exception('confilcet in path to package :', path, "with the key:", first)
     packa = path[arr[0]:]
-    packa =str(packa).replace("/",'.')
-    return  packa[:cut]
+    packa = str(packa).replace("/", '.')
+    return packa[:cut]
+
 
 def main(sys):
-    if len(sys.argv) > 1 :
+    if len(sys.argv) > 1:
         root = sys.argv[1]
         fix_bug_tests(root)
 
-def mkdir_system(path_root,name,is_del=True):
+
+def mkdir_system(path_root, name, is_del=True):
     if path_root is None:
         raise Exception("[Error] passing a None path --> {}".format(path_root))
-    if path_root[-1]!='/':
-        path_root=path_root+'/'
-    if os.path.isdir("{}{}".format(path_root,name)):
+    if path_root[-1] != '/':
+        path_root = path_root + '/'
+    if os.path.isdir("{}{}".format(path_root, name)):
         if is_del:
-            os.system('rm -r {}{}'.format(path_root,name))
+            os.system('rm -r {}{}'.format(path_root, name))
         else:
-            print "{}{} is already exist".format(path_root,name)
+            print "{}{} is already exist".format(path_root, name)
             return '{}{}'.format(path_root, name)
-    os.system('mkdir {}{}'.format(path_root,name))
-    return '{}{}'.format(path_root,name)
+    os.system('mkdir {}{}'.format(path_root, name))
+    return '{}{}'.format(path_root, name)
+
+
 
 if __name__ == "__main__":
     main(sys)
