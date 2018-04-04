4117e62a0ea2a4a4f9af24385ededc0a6cbb75dc
eranhe <eran505@gmail.com>
2018-04-04 14:15:23 +0300

    04/04 14:15:23

--- bash_scripts/f_wrapper_pit.sh
+++ bash_scripts/f_wrapper_pit.sh
@@ -7,13 +7,11 @@ arg_package=${1}
 ATG="/home/ise/eran/repo/ATG/"
 Mathjar="/home/ise/eran/repo/common_math/jars/commons-math3-3.5-src.tar.gz"
 father_dir=${PWD}
-${ATG}python pit_render_test.py ${PWD}
 for D in `find ${PWD}  -maxdepth 1  -type d  `
 do
 
     dir_name=${D##*/}
     if [[ $dir_name == *"_t="* ]]; then
-
 	    string=${D}
 	    array+=(${D})
 	fi
@@ -31,16 +29,20 @@ touch "${PWD}/logs/"${file_log}
 file_log=${PWD}"/logs/"${file_log}
 
 cnt=${#array[@]}
+echo "_____${cnt}________"
 for ((i=0;i<cnt;i++)); do
-	array_sub=()
 	father_dir=${array[i]}
+	array_sub=()
+	echo "f dir ="${father_dir}
 	if [ ! -d "${father_dir}/pit_test" ]; then
 		mkdir "${father_dir}/pit_test"
 	fi
 	for D in `find ${father_dir}  -maxdepth 1  -type d  `
 	do
 		currnet_dir=${D##*/}
+		echo "currnet_dir= "${currnet_dir}
 		if [[ $currnet_dir == *"exp"* ]]; then
+			echo ${D}
 	    	string=${D}
 	    	array_sub+=(${D})
 		fi
@@ -48,8 +50,8 @@ for ((i=0;i<cnt;i++)); do
 	#suffix_name=${path_dir##*/}
 	#echo "path_dir"$path_dir
 	#echo "path_dir="$suffix_name
-	cnt=${#array_sub[@]}
-	for ((k=0;k<cnt;k++)); do
+	inner_loop=${#array_sub[@]}
+	for ((k=0;k<inner_loop;k++)); do
 		now_dir=${array_sub[k]}
 		echo "now="$now_dir
 		suffix_name=${now_dir##*/}
@@ -63,6 +65,9 @@ for ((i=0;i<cnt;i++)); do
 		string_dir="ALL_"${mod}"_${time}_"${it}"_"
 		echo $string_dir
 		cut_dir=${father_dir}/"pit_test"/${string_dir}
+		if [ -d "${cut_dir}" ]; then
+			rm -r ${cut_dir}
+		fi
 		mkdir $cut_dir
 		tar -xzf ${Mathjar} -C $cut_dir
 		relative_path=${cut_dir}/commons-math3-3.5-src/
@@ -73,11 +78,13 @@ for ((i=0;i<cnt;i++)); do
 		cp -ar ${ATG}pti_init.py ${relative_path}
 		cd ${relative_path}
 		mvn -fn install
-
 	done
 
 done
 
+echo '\n'
+echo "---- Done building the environment-----"
+echo '\n'
 ##########################3
 
 python ${ATG}pit_render_test.py ${root_dir}
