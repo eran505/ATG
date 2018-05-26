be33f929b883c1acd4e9e5024f961514260d9510
eranhe <eran505@gmail.com>
2018-05-27 00:56:11 +0300

    05/27 00:56:11

---
+++ bash_scripts/D4j_init.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+ATG="/home/ise/eran/repo/ATG/"
+
+python ${ATG}defects_lib.py Math /home/ise/Desktop/defect4j_exmple/out/ evosuite-1.0.5.jar /home/ise/eran/evosuite/jar/ 2 1 100 True class
+
