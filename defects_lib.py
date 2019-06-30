4e9b408809198e3373c4dcfc85f0ebb6a0c4f2bb
eranhe <eran505@gmail.com>
2019-06-30 15:23:34 +0300

    06/30 15:23:33

--- defects_lib.py
+++ defects_lib.py
@@ -2577,11 +2577,12 @@ if __name__ == "__main__":
     '''
     before_op()
 
-    #args ='py. d4j -i /home/ise/eran/D4J/info/ -M U -C 0 -d /home/ise/programs/defects4j/framework/bin -b 5 -r 90 -o /home/ise/eran/D4j/out/ -t test_trace -p Math -k U'
+    args ='py. d4j -i /home/ise/eran/D4J/info/ -M U -C 0 -d /home/ise/programs/defects4j/framework/bin -b 5 -r 90 -o /home/ise/eran/D4j/out/ -t test_trace -p Math -k U'
     #args = 'pp re_test /home/ise/eran/D4j/out/Math'
     #args = 'py map_test /home/ise/eran/D4j/out'
     #args = 'py map_test'
-    #sys.argv=args.split()
+    #args = 'py re_test'
+    sys.argv=args.split()
     main_parser()
     # fixer_maven(p_path)
     # main_wrapper(args)
