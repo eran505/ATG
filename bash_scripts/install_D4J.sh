c36ea518639975ed2f26472bd6b59e3efb2bb0ab
eranhe <eran505@gmail.com>
2019-07-01 16:11:55 +0300

    07/01 16:11:55

---
+++ bash_scripts/install_D4J.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+sudo apt-get install subversion -y
+cd
+DIRECTORY='/home/ise/programs'
+if [ ! -d "$DIRECTORY" ]; then
+	# Control will enter here if $DIRECTORY doesn't exist.
+	mkdir ${DIRECTORY} 	
+fi
+
+cd ${DIRECTORY}
+git clone https://github.com/rjust/defects4j
+cd defects4j
+bash ./init.sh
+sudo apt-get install libdbi-perl
+sudo cpan Bundle::CSV DBD::CSV DBI
+exit
