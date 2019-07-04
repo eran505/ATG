259cc4bdc0cc9649b72c718c687a539e51924078
eranhe <eran505@gmail.com>
2019-07-04 16:41:23 +0300

    07/04 16:41:23

--- bash_scripts/eran_D4j.sh
+++ bash_scripts/eran_D4j.sh
@@ -3,17 +3,13 @@
 ATG="/home/ise/eran/repo/ATG/"
 p_name='Lang'
 
-sudo chmod -R 777 /home/ise/eran/eran_D4j/
-
-python ${ATG}defects_lib.py -p ${p_name} -F fix -o /home/ise/eran/eran_D4j/${p_name}_t=2 -e /home/ise/eran/evosuite/jar/evosuite-1.0.5.jar -b 180 -l 1 -u 100 -t package -c F -k U -r 2-400 -M U -f F
-
-python ${ATG}defects_lib.py -p ${p_name} -F fix -o /home/ise/eran/eran_D4j/${p_name}_t=5 -e /home/ise/eran/evosuite/jar/evosuite-1.0.5.jar -b 180 -l 1 -u 100 -t package -c F -k U -r 2-400 -M U -f F
-
-python ${ATG}defects_lib.py -p ${p_name} -F fix -o /home/ise/eran/eran_D4j/${p_name}_t=10 -e /home/ise/eran/evosuite/jar/evosuite-1.0.5.jar -b 180 -l 1 -u 100 -t package -c F -k U -r 2-400 -M U -f F
-
-python ${ATG}defects_lib.py -p ${p_name} -F fix -o /home/ise/eran/eran_D4j/${p_name}_t=20 -e /home/ise/eran/evosuite/jar/evosuite-1.0.5.jar -b 20 -l 1 -u 100 -t package -c F -k U -r 2-400 -M U -f F
-
-python ${ATG}defects_lib.py -p ${p_name} -F fix -o /home/ise/eran/eran_D4j/${p_name}_t=50 -e /home/ise/eran/evosuite/jar/evosuite-1.0.5.jar -b 50 -l 1 -u 100 -t package -c F -k U -r 2-400 -M U -f F
-
-python ${ATG}defects_lib.py -p ${p_name} -F fix -o /home/ise/eran/eran_D4j/${p_name}_t=90 -e /home/ise/eran/evosuite/jar/evosuite-1.0.5.jar -b 90 -l 1 -u 100 -t package -c F -k U -r 2-400 -M U -f F
-
+sudo env "PATH=$PATH" python ${ATG}defects_lib.py d4j -i /home/ise/eran/D4J/info/ -M U -C 0 -d /home/ise/programs/defects4j/framework/bin -q 1 -b 180 -r 1-65 -o /home/ise/eran/D4j/out/ -t package_only -p ${p_name} -k U
+sudo env "PATH=$PATH" python ${ATG}defects_lib.py d4j -i /home/ise/eran/D4J/info/ -M U -C 0 -d /home/ise/programs/defects4j/framework/bin -q 1 -b 180 -r 1-65 -o /home/ise/eran/D4j/out/ -t package_only -p ${p_name} -k U
+sudo env "PATH=$PATH" python ${ATG}defects_lib.py d4j -i /home/ise/eran/D4J/info/ -M U -C 0 -d /home/ise/programs/defects4j/framework/bin -q 1 -b 180 -r 1-65 -o /home/ise/eran/D4j/out/ -t package_only -p ${p_name} -k U
+sudo env "PATH=$PATH" python ${ATG}defects_lib.py d4j -i /home/ise/eran/D4J/info/ -M U -C 0 -d /home/ise/programs/defects4j/framework/bin -q 1 -b 180 -r 1-65 -o /home/ise/eran/D4j/out/ -t package_only -p ${p_name} -k U
+sudo env "PATH=$PATH" python ${ATG}defects_lib.py d4j -i /home/ise/eran/D4J/info/ -M U -C 0 -d /home/ise/programs/defects4j/framework/bin -q 1 -b 180 -r 1-65 -o /home/ise/eran/D4j/out/ -t package_only -p ${p_name} -k U
+sudo env "PATH=$PATH" python ${ATG}defects_lib.py d4j -i /home/ise/eran/D4J/info/ -M U -C 0 -d /home/ise/programs/defects4j/framework/bin -q 1 -b 180 -r 1-65 -o /home/ise/eran/D4j/out/ -t package_only -p ${p_name} -k U
+sudo env "PATH=$PATH" python ${ATG}defects_lib.py d4j -i /home/ise/eran/D4J/info/ -M U -C 0 -d /home/ise/programs/defects4j/framework/bin -q 1 -b 180 -r 1-65 -o /home/ise/eran/D4j/out/ -t package_only -p ${p_name} -k U
+sudo env "PATH=$PATH" python ${ATG}defects_lib.py d4j -i /home/ise/eran/D4J/info/ -M U -C 0 -d /home/ise/programs/defects4j/framework/bin -q 1 -b 180 -r 1-65 -o /home/ise/eran/D4j/out/ -t package_only -p ${p_name} -k U
+sudo env "PATH=$PATH" python ${ATG}defects_lib.py d4j -i /home/ise/eran/D4J/info/ -M U -C 0 -d /home/ise/programs/defects4j/framework/bin -q 1 -b 180 -r 1-65 -o /home/ise/eran/D4j/out/ -t package_only -p ${p_name} -k U
+sudo env "PATH=$PATH" python ${ATG}defects_lib.py d4j -i /home/ise/eran/D4J/info/ -M U -C 0 -d /home/ise/programs/defects4j/framework/bin -q 1 -b 180 -r 1-65 -o /home/ise/eran/D4j/out/ -t package_only -p ${p_name} -k U
